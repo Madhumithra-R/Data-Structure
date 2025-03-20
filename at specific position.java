5109d9f7c24fee8cac48694fb3979e2c6541bdf5
Madhumithra R <madhumithramithra8@gmail.com>
2025-03-20 15:52:42 +0530

    Add files via upload

---
+++ at specific position.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class position {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n + 1]; 
+        System.out.print("Enter " + n + " elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter element to insert: ");
+        int newElement = sc.nextInt();
+
+        System.out.print("Enter position: ");
+        int pos = sc.nextInt();
+        for (int i = n; i >= pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[pos - 1] = newElement;
+        System.out.print("Updated array: ");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
