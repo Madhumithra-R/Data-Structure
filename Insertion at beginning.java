9e5b179872e5d63b98c7d4f442dd707554eb1a71
Madhumithra R <madhumithramithra8@gmail.com>
2025-03-20 15:55:08 +0530

    Update Insertion at beginning.java

--- Insertion at beginning.java
+++ Insertion at beginning.java
@@ -2,15 +2,11 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("enter array size:");
         int n =sc.nextInt();
-        
         int[] arr = new int[n+1];
-        
         System.out.print("enter element to insert at beginning:");
         int newElement = sc.nextInt();
-        
         System.out.print("enter " + n + " elements:");
         for(int i=0;i<n;i++){
             arr[i+1] = sc.nextInt();
